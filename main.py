967e653f9fb499eb8e4c7d2b667ef6d95b4b3f6e
Deborah Pelacani Cruz <dekape23@gmail.com>
2023-06-21 19:24:44 +0000

    clean main

--- main.py
+++ main.py
@@ -9,35 +9,19 @@ plt.rcParams['image.cmap'] = 'cividis'
 
 if __name__ == "__main__":
 
-    # Guess model
-    x = np.load("/home/dp4018/data/ultrasound-data/Ultrasound-Vp-sagittal-models/vp_100206.npy")[0]
+    # Model for backpropagation -- here we use a version of the true model that only contains the skull
+    x = np.load("/home/dp4018/data/ultrasound-data/Ultrasound-Vp-transverse-models/vp_204622.npy")
     x0 = create_x0_from_true_model(x)
-    fig, axs = plt.subplots(1, 2)
-    axs[0].imshow(x, vmin=1400, vmax=3000)
-    axs[1].imshow(x0, vmin=1400, vmax=3000)
 
-    # Acquisition and name
-    acquisitions_file = "/home/dp4018/data/ultrasound-data/Ultrasound-Vp-sagittal-data/acoustic/data/vp_100206_3shots-360-Acquisitions.h5"
+
+    # Stride dump files and name
+    acquisitions_file = "/home/dp4018/data/ultrasound-data/Ultrasound-Vp-transverse-data/acoustic/data/vp_204622_36shots-512resize-highf-Acquisitions.h5"
+    geometry_file = "/home/dp4018/data/ultrasound-data/Ultrasound-Vp-transverse-data/acoustic/data/vp_204622_36shots-512resize-highf-Geometry.h5"
+    transducers_file = "/home/dp4018/data/ultrasound-data/Ultrasound-Vp-transverse-data/acoustic/data/vp_204622_36shots-512resize-highf-Transducers.h5"
     name = infer_name_from_acquisition_file(acquisitions_file)
     
     # PIDS instatiantion
-    pids = PIDS(acquisitions_file=acquisitions_file, name=name, x0=x0)
-
-    # Save problem of true model for examples figures
-    from stride import *
-    vp = ScalarField(
-        name='vp', grid=pids.problem.grid,
-        data=x,
-        needs_grad=True
-    )
-    pids.problem.medium.add(vp)
-    pids.problem.plot(acquisitions=False, cmap="cividis")
-    plt.savefig(os.path.join("./figures", "example_x_problem.png"))
-
-    # Save mid shot plot of observed data for examples figures
-    mid_shot = pids.problem.acquisitions.shots[int((len(pids.problem.acquisitions.shots) - 1)/2)]
-    mid_shot.plot_observed()
-    plt.savefig(os.path.join("./figures", "example_data.png"))
+    pids = PIDS(acquisitions_file=acquisitions_file, geometry_file=geometry_file, transducers_file=transducers_file, name=name, x0=x0)
 
     # Process and save PIDS data as .npy; save plots of PIDS and Problem as .png
     pids.process()
